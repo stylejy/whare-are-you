73e6cb93a76dbb2a583dbf6ad7e809e8a5b14463
JooYoung Lee <stylejy@gmail.com>
2021-05-01 16:32:41 +0900

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,68 @@ import GoogleMapReact from 'google-map-react';
 import Marker from 'react-spinners/PuffLoader';
 import short from 'short-uuid';
 import useWebSocket from 'react-use-websocket';
+import reset from 'styled-reset';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const socketUrl = 'wss:test.net';
 
-const MarkerComponent = ({lat, lng}: {lat: number, lng: number}) => <Marker color={'#4A90E2'} loading={true} size={20} />
+const MarkerComponent = ({ lat, lng }: { lat: number; lng: number }) => (
+  <Marker color="#4A90E2" loading size={20} />
+);
 
-const SharingNavComponent = ({sharingId, sharingButtonHandler, cancelButtonHandler}: {sharingId: string, sharingButtonHandler: () => void, cancelButtonHandler: () => void}) => (
+const SharingNavComponent = ({
+  sharingId,
+  sharingButtonHandler,
+  cancelButtonHandler,
+}: {
+  sharingId: string;
+  sharingButtonHandler: () => void;
+  cancelButtonHandler: () => void;
+}) => (
   <>
     <li>공유 ID: {sharingId}</li>
     <button onClick={cancelButtonHandler}>공유취소</button>
   </>
-)
+);
 
-const MonitoringNavComponent = ({monitoringId, isStarted, inputChangeHandler, startButtonHandler, cancelButtonHandler}: {monitoringId: string, isStarted: boolean, inputChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void, startButtonHandler: () => void, cancelButtonHandler: () => void}) => {
+const MonitoringNavComponent = ({
+  monitoringId,
+  isStarted,
+  inputChangeHandler,
+  startButtonHandler,
+  cancelButtonHandler,
+}: {
+  monitoringId: string;
+  isStarted: boolean;
+  inputChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+  startButtonHandler: () => void;
+  cancelButtonHandler: () => void;
+}) => {
   if (isStarted && monitoringId) {
     return (
       <>
         <li>공유 ID ( {monitoringId} ) 의 동선을 트래킹 중입니다. </li>
         <button onClick={cancelButtonHandler}>돌아가기</button>
       </>
-    )
+    );
   }
   return (
     <>
       <li>공유 ID를 입력해주세요: </li>
-      <input value={monitoringId} onChange={inputChangeHandler}></input>
+      <input value={monitoringId} onChange={inputChangeHandler} />
       <button onClick={startButtonHandler}>모니터 시작</button>
       <button onClick={cancelButtonHandler}>돌아가기</button>
     </>
-  )
-}
+  );
+};
 
 function App() {
-  const [position, setPosition] = useState({lat: 0, lng: 0})
-  const [sharingId, setSharingId] = useState('')
-  const [monitoringId, setMonitoringId] = useState('')
-  const [isStarted, setIsStarted] = useState(false)
-  const [isSharingMode, setIsSharingMode] = useState(false)
-  const [isMonitoringMode, setIsMonitoringMode] = useState(false)
+  const [position, setPosition] = useState({ lat: 0, lng: 0 });
+  const [sharingId, setSharingId] = useState('');
+  const [monitoringId, setMonitoringId] = useState('');
+  const [isStarted, setIsStarted] = useState(false);
+  const [isSharingMode, setIsSharingMode] = useState(false);
+  const [isMonitoringMode, setIsMonitoringMode] = useState(false);
 
   const {
     sendMessage,
@@ -49,71 +73,65 @@ function App() {
     lastMessage,
     lastJsonMessage,
     readyState,
-    getWebSocket
+    getWebSocket,
   } = useWebSocket(socketUrl, {
     onOpen: () => console.log('socket opened'),
-    shouldReconnect: (closeEvent) => true,
+    shouldReconnect: closeEvent => true,
   });
-  
+
   useEffect(() => {
-    let sendCoordsInterval: NodeJS.Timeout
+    let sendCoordsInterval: NodeJS.Timeout;
     if (isSharingMode) {
       sendCoordsInterval = setInterval(() => {
         /*
           sendMessage 를 할 수 있도록 한다.
         */
-      }, 1000)
+      }, 1000);
     }
     const mapInfoElement = document.getElementById('Map-info');
-    navigator.geolocation.getCurrentPosition((position) => {
-      const [lat, lng] = [
-        position.coords.latitude,
-        position.coords.longitude
-      ]
-      setPosition({lat, lng})
+    navigator.geolocation.getCurrentPosition(position => {
+      const [lat, lng] = [position.coords.latitude, position.coords.longitude];
+      setPosition({ lat, lng });
       if (mapInfoElement) {
         mapInfoElement.innerHTML = `lat: ${lat}, lng: ${lng}`;
       }
-    })
-    navigator.geolocation.watchPosition((position) => { 
-      const [lat, lng] = [
-        position.coords.latitude,
-        position.coords.longitude
-      ]
-      setPosition({lat, lng})
+    });
+    navigator.geolocation.watchPosition(position => {
+      const [lat, lng] = [position.coords.latitude, position.coords.longitude];
+      setPosition({ lat, lng });
       if (mapInfoElement) {
         mapInfoElement.innerHTML = `lat: ${lat}, lng: ${lng}`;
       }
     });
 
     return () => {
-      clearInterval(sendCoordsInterval)
-    }
+      clearInterval(sendCoordsInterval);
+    };
   });
 
   const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
-    setMonitoringId(event.target.value)
-  }
+    setMonitoringId(event.target.value);
+  };
 
   const onClickStartButton = () => {
-    setIsStarted(true)
-  }
+    setIsStarted(true);
+  };
 
   const onClickShareButton = () => {
-    setSharingId(short.generate())
-    setIsSharingMode(true)
-  }
+    setSharingId(short.generate());
+    setIsSharingMode(true);
+  };
 
   const onClickMonitorButton = () => {
-    setIsMonitoringMode(true)
-  }
+    setIsMonitoringMode(true);
+  };
 
   const onClickCancelButton = () => {
     setIsSharingMode(false);
     setIsMonitoringMode(false);
     setIsStarted(false);
     setMonitoringId('');
-  }
+  };
 
   return (
     <div className="App">
@@ -125,18 +143,32 @@ function App() {
       </header>
       <nav>
         <ul>
-          {
-            !isSharingMode && !isMonitoringMode ? <>
-              <li className="Button" onClick={onClickShareButton}>내 위치 공유</li>
-              <li className="Button" onClick={onClickMonitorButton}>위치 확인</li>
-            </> : null
-          }
-          {
-            isSharingMode ? SharingNavComponent({sharingId, sharingButtonHandler: onClickShareButton, cancelButtonHandler: onClickCancelButton}) : null
-          }
-          {
-            isMonitoringMode ? MonitoringNavComponent({monitoringId, isStarted, inputChangeHandler: onChangeInput, startButtonHandler: onClickStartButton, cancelButtonHandler: onClickCancelButton}) : null
-          }
+          {!isSharingMode && !isMonitoringMode ? (
+            <>
+              <li className="Button" onClick={onClickShareButton}>
+                내 위치 공유
+              </li>
+              <li className="Button" onClick={onClickMonitorButton}>
+                위치 확인
+              </li>
+            </>
+          ) : null}
+          {isSharingMode
+            ? SharingNavComponent({
+                sharingId,
+                sharingButtonHandler: onClickShareButton,
+                cancelButtonHandler: onClickCancelButton,
+              })
+            : null}
+          {isMonitoringMode
+            ? MonitoringNavComponent({
+                monitoringId,
+                isStarted,
+                inputChangeHandler: onChangeInput,
+                startButtonHandler: onClickStartButton,
+                cancelButtonHandler: onClickCancelButton,
+              })
+            : null}
         </ul>
       </nav>
       <section className="App-section">
@@ -145,9 +177,14 @@ function App() {
           center={position}
           defaultZoom={17}
         >
-          {
-            isMonitoringMode ? <MarkerComponent lat={lastJsonMessage.lat} lng={lastJsonMessage.lng} /> : <MarkerComponent lat={position.lat} lng={position.lng} />
-          }     
+          {isMonitoringMode ? (
+            <MarkerComponent
+              lat={lastJsonMessage.lat}
+              lng={lastJsonMessage.lng}
+            />
+          ) : (
+            <MarkerComponent lat={position.lat} lng={position.lng} />
+          )}
         </GoogleMapReact>
       </section>
     </div>
